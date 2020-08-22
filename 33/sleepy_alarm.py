a9cef3406417ead2928f7ae1628c6d145dc32264
David Gutierrez <david@pythonsoftware.solutions>
2020-08-22 11:29:58 -0400

    initial commit

---
+++ 33/sleepy_alarm.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+async def sleepy_alarm(person, time):
+   await asyncio.sleep(time)
+   print(f"{person} -- wake up!")
+
+
+async def wake_up_gang():
+    tasks = [
+        asyncio.create_task(sleepy_alarm("Bob", 3), name="wake up Bob"),
+        asyncio.create_task(sleepy_alarm("Sanjeet", 4), name="wake up Sanjeet"),
+        asyncio.create_task(sleepy_alarm("Doris", 2), name="wake up Doris"),
+        asyncio.create_task(sleepy_alarm("Kim", 4), name="wake up Kim")
+    ]
+    await asyncio.gather(*tasks)
+
+asyncio.run(wake_up_gang())
